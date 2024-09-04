3c917592d3bcd903184813882db428742c785abf
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-04 11:00:54 -0500

    Added function to fetch API data upon page load

--- src/App.js
+++ src/App.js
@@ -8,6 +8,22 @@ const App = () => {
   const [data, setData] = useState({});
   const [status, setStatus] = useState("loading");
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const request = await fetch("https://randomuser.me/api/");
+        // const response = await request.json();
+        // const results = response.results;    // Assigns response to the results object property
+        const {results} = await request.json(); // This line does the same thing as the above two lines together by destructuring the results immediately from the response
+        setData(results[0]);
+        console.log(data);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <UserDetails />
