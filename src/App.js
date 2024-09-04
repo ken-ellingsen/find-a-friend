bec0122292b40c8fd257ceb7d755d5b4c1c9a49d
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-04 11:31:54 -0500

    Feature: added usability to 'Fetch Another' button

--- src/App.js
+++ src/App.js
@@ -8,23 +8,24 @@ const App = () => {
   const [data, setData] = useState({});
   const [status, setStatus] = useState("loading");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setStatus("loading");
-        const request = await fetch("https://randomuser.me/api/");
-        // const response = await request.json();
-        // const results = response.results;    // Assigns response to the results object property
-        const {results} = await request.json(); // This line does the same thing as the above two lines together by destructuring the results immediately from the response
-        if (request.status === 200) {
-          setData(results[0]);
-          setStatus("success");
-        }
-      } catch (e) {
-        console.log(e);
-        setStatus("error");
+  const fetchData = async () => {
+    try {
+      setStatus("loading");
+      const request = await fetch("https://randomuser.me/api/");
+      // const response = await request.json();
+      // const results = response.results;    // Assigns response to the results object property
+      const {results} = await request.json(); // This line does the same thing as the above two lines together by destructuring the results immediately from the response
+      if (request.status === 200) {
+        setData(results[0]);
+        setStatus("success");
       }
+    } catch (e) {
+      console.log(e);
+      setStatus("error");
     }
+  }
+
+  useEffect(() => {
     fetchData();
   }, []);
 
@@ -33,7 +34,7 @@ const App = () => {
   return (
     <div className="App">
       {status === "loading" && <Loader />}
-      {status === "success" && <UserDetails data={data} />}
+      {status === "success" && <UserDetails data={data} fetchData={fetchData} />}
       {status === "error" && "Something went wrong."}
     </div>
   );
--- src/UserDetails.js
+++ src/UserDetails.js
@@ -1,10 +1,10 @@
-const UserDetails = ( {data} ) => (
+const UserDetails = ( {data, fetchData} ) => (
   <div className="user-details">
-    <p className="name ">{data.name.title}. {data.name.first} {data.name.last}</p>
+    <p className="name ">{data.name.title} {data.name.first} {data.name.last}</p>
     <p className="country">{data.nat}</p>
     <p className="email">{data.email}</p>
     <figure><img src={data.picture.large} alt="Random user avatar" /></figure>
-    <button>Fetch Another</button>
+    <button onClick={fetchData}>Fetch Another</button>
   </div>
 );
 
